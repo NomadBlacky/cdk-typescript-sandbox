a2a7f7c4a55c81e59064e93c974651f36af16aea
NomadBlacky <tkadowaki.dev.blackey@gmail.com>
2019-09-04 02:30:40 +0900

    Add a nginx task on LoadBalancedFargateService

--- lib/my-app-stack.ts
+++ lib/my-app-stack.ts
@@ -1,9 +1,11 @@
 import cdk = require("@aws-cdk/core");
 import ec2 = require("@aws-cdk/aws-ec2");
 import ecs = require("@aws-cdk/aws-ecs");
+import patterns = require("@aws-cdk/aws-ecs-patterns");
+//import route53 = require("@aws-cdk/aws-route53");
 
 export class MyAppStack extends cdk.Stack {
-    constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+    constructor(scope: cdk.Construct, id: string, props: cdk.StackProps) {
         super(scope, id, props);
 
         const vpc = new ec2.Vpc(this, "ExampleVPC", {
@@ -12,6 +14,18 @@ export class MyAppStack extends cdk.Stack {
 
         const cluster = new ecs.Cluster(this, "MyAppCluster", {
             vpc: vpc
+        });
+
+        new patterns.LoadBalancedFargateService(this, "MyFagateService", {
+            cluster: cluster,
+            cpu: 256,
+            memoryLimitMiB: 512,
+            desiredCount: 1,
+            image: ecs.ContainerImage.fromRegistry('nginx'),
+            publicLoadBalancer: true,
+            containerPort: 80,
+            // domainName: 'cdk-typescript-sandbox.nomadblacky.dev.',
+            // domainZone: route53.HostedZone.fromHostedZoneId(this, 'MyZone', 'xxxxxxxx')
         });
     }
 }
