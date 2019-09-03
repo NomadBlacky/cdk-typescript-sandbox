3adf78b05308d82538337668b717e9ecd5dc1b3a
NomadBlacky <tkadowaki.dev.blackey@gmail.com>
2019-09-04 01:14:32 +0900

    Add ECS Cluster

--- lib/my-app-stack.ts
+++ lib/my-app-stack.ts
@@ -1,12 +1,17 @@
 import cdk = require("@aws-cdk/core");
 import ec2 = require("@aws-cdk/aws-ec2");
+import ecs = require("@aws-cdk/aws-ecs");
 
 export class MyAppStack extends cdk.Stack {
     constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
         super(scope, id, props);
 
-        new ec2.Vpc(this, "ExampleVPC", {
+        const vpc = new ec2.Vpc(this, "ExampleVPC", {
             maxAzs: 2
+        });
+
+        const cluster = new ecs.Cluster(this, "MyAppCluster", {
+            vpc: vpc
         });
     }
 }
